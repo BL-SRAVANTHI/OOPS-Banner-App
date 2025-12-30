3893a1d0b47d4cd0454ef8d35a671522c74c605a
Sravanthi <sravanthi.kallavai@bridgelabz.com>
2025-12-30 09:56:41 +0530

    Display OOPs in Console

---
+++ src/main/java/com/bridgelabz/oops_banner_app/OOPSBannerApp.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.oops_banner_app;
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        String text = "OOPS";
+        System.out.println(text);
+    }
+}
