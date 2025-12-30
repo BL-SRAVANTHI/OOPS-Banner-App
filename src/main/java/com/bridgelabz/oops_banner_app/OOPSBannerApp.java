2f1d69b43799578c98b9d94064ec4fac99d24ebc
Sravanthi <sravanthi.kallavai@bridgelabz.com>
2025-12-30 15:01:05 +0530

    Display OOPS as banner of stars and spaces

--- src/main/java/com/bridgelabz/oops_banner_app/OOPSBannerApp.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.oops_banner_app;
-
-public class OOPSBannerApp {
-    public static void main(String[] args) {
-        String text = "OOPS";
-        System.out.println(text);
-    }
-}
---
+++ src/main/java/com/bridgelabz/oopsbannerapp/OOPSBannerApp.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.oopsbannerapp;
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        String[] lines = {
+                String.join("  ",
+                        "   ***  ", "    ***  ", " ****** ", "     *****"),
+                String.join("  ",
+                        " **   **", "  **   **", " **    **", "  **    "),
+                String.join("  ",
+                        "**     **", "**     **", "** ", "  **", "**"),
+                String.join("  ",
+                        "**     **", "**     **", "**    **", "   **   "),
+                String.join("  ",
+                        "**     **", "**     **", "******    ", "   ***"),
+                String.join("  ",
+                        "**     **", "**     **", "**    ", "          **"),
+                String.join("  ",
+                        "**     **", "**     **", "**    ", "           **"),
+                String.join("  ",
+                        " **   **", "  **   **", " **    ", "          **"),
+                String.join("  ",
+                        "   ***   ", "   ***   ", "**    ", "     *****")
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
